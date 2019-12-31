76461536182541b4eaa73766d4a7a62a7d5cd2df
David Johnson <djohnson.m@gmail.com>
2019-12-30 19:55:50 -0600

    Remove binary.

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use selection_sort::selection_sort;
-
-pub fn main () {
-    let mut vec = vec![10,8,9,6,7,4,5,2,3,1,0];
-    println!("{:?}", vec);
-    selection_sort (&mut vec);
-    println!("length: {}", vec.len());
-    println!("{:?}", vec);
-}
